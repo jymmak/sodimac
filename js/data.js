aaea6763420225bf73f304ff1e20791cea183e11
andreatelleria <andrea.telleria@gmail.com>
2018-04-11 17:33:56 -0500

    Agregando data

---
+++ js/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    proveedor: 'Pacasmayo',
+    categoría: 'cemento',
+    cantidad: '100',
+    código: '6193'
+  },
+  {
+    proveedor: 'Pirámide',
+    categoría: 'ladrillos',
+    cantidad: '200',
+    código: '2074'
+  },
+  {
+    proveedor: 'Goodyear',
+    categoría: 'llantas',
+    cantidad: '150',
+    código: '9136'
+  },
+  {
+    proveedor: 'Dewalt',
+    categoría: 'taladros',
+    cantidad: '200',
+    código: '2864'
+  },
+  {
+    proveedor: 'Home Collection',
+    categoría: 'alfombras',
+    cantidad: '500',
+    código: '1420'
+  }
+];
+
+console.log(data);
