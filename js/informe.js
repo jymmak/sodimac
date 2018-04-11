f48d1cfcc5d274e611c1a106e5ab5a77b543e77a
wendytamara <anacarlavegam@gmail.com>
2018-04-11 17:40:05 -0500

    añadiendo vista formulario de informe

---
+++ js/informe.js
@@ -0,0 +1,39 @@
+$('.ui.form')
+  .form({
+    fields: {
+
+      name: {
+        identifier: 'proveedor',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Ingrese nombre del proveedor'
+          }
+        ]
+      },
+
+      category: {
+        identifier: 'category',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Ingrese categoria'
+          }
+        ]
+      },
+
+ 
+ 
+      quantity: {
+        identifier: 'quantity',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Ingrese cantidad'
+          }
+        ]
+      },
+
+    }
+  })
+;
