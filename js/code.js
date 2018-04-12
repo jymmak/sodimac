fcd4d0f4ba0f23fa4183723c04950bc5c3e53b4e
wendytamara <anacarlavegam@gmail.com>
2018-04-11 22:19:45 -0500

    finalizando validacion data

--- js/code.js
+++ js/code.js
@@ -1,9 +1,19 @@
 
 var btnCode = $('#btnCode');
 var code = $('#code');
+var btnEnviarForm = $('#btnEnviarForm');
+var provider = $('#provider');
+var category = $('#category');
+var quantity = $('#quantity');
+
+
+
+
+$('#informCode').html(localStorage.code);
 
 btnCode.on('click', validCode);
 code.on('keyup', validCodeOn)
+btnEnviarForm.on('click', submitForm);
 
 
  function validCode(event) {
@@ -12,19 +22,49 @@ code.on('keyup', validCodeOn)
 }
    
   function validCodeOn() {
-
     data.map(function (element) {
-      console.log(element.codigo)
-    debugger
+    
       if(code.val() == element.codigo) {
-        btnCode.removeClass('disabled');
+        localStorage.code = code.val()
+        console.log(localStorage.code)
+        btnCode.removeClass('disabled');      
 
-        
-
-      }
-     
+      }     
     });
 
   }
 
+  function submitForm(event) {
+    event.preventDefault();
+    
+    data.map(function (element) {
+  
+
+      if(localStorage.code == element.codigo && provider.val() == element.proveedor && category.val() == element.categoría && quantity.val() == element.cantidad  ) {
+
+        alert('datos correctos, espere un  momento')
+        window.location.href = 'finish.html'
+
+        localStorage.provider = provider.val();
+        localStorage.category = category.val();
+        localStorage.quantity = quantity.val();
+        console.log(localStorage.provider);
+        console.log(localStorage.category);
+        console.log(localStorage.quantity);
+
+        
+       
+
+
+      }     
+    });
+
+    
+  }
+
+
+ 
+
+
+  
    
--- js/data.js
+++ js/data.js
@@ -1,24 +1,24 @@
 const data = [
   {
-    proveedor: 'Pacasmayo',
+    proveedor: 'pacasmayo',
     categoría: 'cemento',
     cantidad: '100',
     codigo: '6193'
   },
   {
-    proveedor: 'Pirámide',
+    proveedor: 'piramide',
     categoría: 'ladrillos',
     cantidad: '200',
     codigo: '2074'
   },
   {
-    proveedor: 'Goodyear',
+    proveedor: 'goodyear',
     categoría: 'llantas',
     cantidad: '150',
     codigo: '9136'
   },
   {
-    proveedor: 'Dewalt',
+    proveedor: 'dewalt',
     categoría: 'taladros',
     cantidad: '200',
     codigo: '2864'
