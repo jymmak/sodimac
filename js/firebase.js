f8fbb29c2c270f09b8d18fab99ba5b9f0267e348
wendytamara <anacarlavegam@gmail.com>
2018-04-12 17:19:37 -0500

    dd

--- js/firebase.js
+++ js/firebase.js
@@ -10,6 +10,8 @@ var contenedor = $('#contenedor');
 var refConvalidaciones = firebase.database().ref().child('chat');
 
 console.log(localStorage.state)
+var hour = getTime();
+console.log(hour);
 
 // funcion al boton enviar
 
@@ -20,6 +22,9 @@ btnEnviarForm.on('click', function(){
   var luna = quantity.val();
   var code = localStorage.code;
   var state = localStorage.state;
+  var hour = getTime();
+
+  
 
 
 // ingresando a database .ref (rama a la que quiero ir) --- push es para ingresar datos a mi arbol
@@ -28,7 +33,8 @@ btnEnviarForm.on('click', function(){
     categoría:mensaje,
     quantity:luna,
     codigo:code,
-    estado:state
+    estado:state,
+    hora:hour
   });
 });
 
@@ -44,9 +50,10 @@ firebase.database().ref('chat')
     let quantity = element.quantity;
     let state = element.estado;
     let code = element.codigo;
+    let hour = element.hora;
     
     html += `<div class="ui vertical menu">
-    <div class="item">
+    <div class="item container-item">
       <div class="header"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">INFORME</font></font></div>
       <div class="menu">
         <a class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Proveedor: ${proveedor} </font></font></a>
@@ -54,9 +61,19 @@ firebase.database().ref('chat')
         <a class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Cantidad: ${quantity} </font></font></a>
         <a class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Estado: ${state} </font></font></a>
         <a class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Codigo: ${code} </font></font></a>
+        <a class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hora: ${hour} </font></font></a>
         </div>
     </div>`;
   });
   contenedor.html(html);
 });
 
+
+
+
+function getTime() {
+  var currentDate = new Date();
+  var hh = currentDate.getHours();
+  var mm = currentDate.getMinutes();
+  return hh + ':' + ((mm < 10 ? '0' : '') + mm);
+}
